182c334e6d5315be87591469b3f976b03c7c7cdd
CCBro <gujikplmaedtchvjnligfyjkhcst@gmail.com>
2022-05-24 00:00:00 +0800

    substrate project init

---
+++ node/src/main.rs
@@ -0,0 +1,15 @@
+//! Armonia Eva Node CLI library.
+#![warn(missing_docs)]
+#![allow(clippy::type_complexity)]
+
+mod chain_spec;
+#[macro_use]
+mod service;
+mod cli;
+mod command;
+mod command_helper;
+mod rpc;
+
+fn main() -> sc_cli::Result<()> {
+    command::run()
+}
