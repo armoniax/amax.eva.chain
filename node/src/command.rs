3389e3973a9110b09062c80a32a04d20095b5e6f
tento <106095289+iamtento@users.noreply.github.com>
2022-06-08 19:13:09 +0800

    Update substrate/frontier to polkadot v0.9.22 (#9)
    * Update substrate/frontier to polkadot-v0.9.22
    
    * Fix compilation error
    
    * Fix runtime-benchmarks feature
    
    * Remove open_frontier_backend

--- node/src/command.rs
+++ node/src/command.rs
@@ -1,10 +1,13 @@
+// Substrate
 use sc_cli::{ChainSpec, RuntimeVersion, SubstrateCli};
 use sc_service::{DatabaseSource, PartialComponents};
+// Frontier
+use fc_db::frontier_database_dir;
 
 use crate::{
     chain_spec,
     cli::{Cli, Subcommand},
-    service::{self, frontier_database_dir},
+    service::{self, db_config_dir},
 };
 
 impl SubstrateCli for Cli {
@@ -93,16 +96,18 @@ pub fn run() -> sc_cli::Result<()> {
             let runner = cli.create_runner(cmd)?;
             runner.sync_run(|config| {
                 // Remove Frontier offchain db
+                let db_config_dir = db_config_dir(&config);
                 let frontier_database_config = match config.database {
                     DatabaseSource::RocksDb { .. } => DatabaseSource::RocksDb {
-                        path: frontier_database_dir(&config, "db"),
+                        path: frontier_database_dir(&db_config_dir, "db"),
                         cache_size: 0,
                     },
                     DatabaseSource::ParityDb { .. } => DatabaseSource::ParityDb {
-                        path: frontier_database_dir(&config, "paritydb"),
+                        path: frontier_database_dir(&db_config_dir, "paritydb"),
                     },
-                    _ =>
-                        return Err(format!("Cannot purge `{:?}` database", config.database).into()),
+                    _ => {
+                        return Err(format!("Cannot purge `{:?}` database", config.database).into())
+                    },
                 };
                 cmd.run(frontier_database_config)?;
                 cmd.run(config.database)
@@ -123,7 +128,7 @@ pub fn run() -> sc_cli::Result<()> {
         #[cfg(feature = "runtime-benchmarks")]
         Some(Subcommand::Benchmark(cmd)) => {
             use crate::command_helper::{inherent_benchmark_data, BenchmarkExtrinsicBuilder};
-            use frame_benchmarking_cli::BenchmarkCmd;
+            use frame_benchmarking_cli::{BenchmarkCmd, SUBSTRATE_REFERENCE_HARDWARE};
             use std::sync::Arc;
 
             let runner = cli.create_runner(cmd)?;
@@ -134,8 +139,9 @@ pub fn run() -> sc_cli::Result<()> {
                 // This switch needs to be in the client, since the client decides
                 // which sub-commands it wants to support.
                 match cmd {
-                    BenchmarkCmd::Pallet(cmd) =>
-                        cmd.run::<amax_eva_runtime::Block, service::ExecutorDispatch>(config),
+                    BenchmarkCmd::Pallet(cmd) => {
+                        cmd.run::<amax_eva_runtime::Block, service::ExecutorDispatch>(config)
+                    },
                     BenchmarkCmd::Block(cmd) => cmd.run(client),
                     BenchmarkCmd::Storage(cmd) => {
                         let db = backend.expose_db();
@@ -145,6 +151,9 @@ pub fn run() -> sc_cli::Result<()> {
                     BenchmarkCmd::Overhead(cmd) => {
                         let ext_builder = BenchmarkExtrinsicBuilder::new(client.clone());
                         cmd.run(config, client, inherent_benchmark_data()?, Arc::new(ext_builder))
+                    },
+                    BenchmarkCmd::Machine(cmd) => {
+                        cmd.run(&config, SUBSTRATE_REFERENCE_HARDWARE.clone())
                     },
                 }
             })
