bc81c5381d2d2b4cb6b55db40e569f29d087b87a
chain-developer <55024141+chain-developer@users.noreply.github.com>
2022-06-21 21:40:56 +0800

    Add ts-tests from frontier (#12)
    * add ts-test from frontier
    
    * fix time support for manual-seal mode
    
    * fix the tests by account genesis change
    
    * fix all testcase failed by change by chain
    
    * add rpc_binary_search_estimate for tests
    
    * fix errors casuse by new version
    
    * add gas tests from 0.9.22
    
    * Add CI for ts-tests
    
    * Fix lint workflow
    
    * Add ts-tests from upstream
    
    * Fix tests
    
    * Skip some tests
    
    * Update ts-tests
    
    Co-authored-by: tento <tento@koushiro.me>

---
+++ ts-tests/tests/config.ts
@@ -0,0 +1,17 @@
+export const GENESIS_ACCOUNT = "0xf24ff3a9cf04c71dbc94d0b566f7a27b94566cac";
+export const GENESIS_ACCOUNT_PRIVATE_KEY = "0x5fb92d6e98884f76de468fa3f6278f8807c48bebc13595d45af5bdc4da702133";
+export const GENESIS_ACCOUNT_BALANCE = "100000000000000000000000";
+
+export const FIRST_CONTRACT_ADDRESS = "0xc01Ee7f10EA4aF4673cFff62710E1D7792aBa8f3";
+
+export const NODE_BINARY_NAME = "amax-eva";
+
+export const RUNTIME_SPEC_NAME = "armonia-wall-e";
+export const RUNTIME_SPEC_VERSION = 1;
+export const RUNTIME_IMPL_VERSION = 1;
+
+export const CHAIN_ID = 161; // wall-e chain id
+export const BLOCK_TIMESTAMP = 2; // 2 seconds per block
+export const BLOCK_HASH_COUNT = 2400;
+export const EXISTENTIAL_DEPOSIT = 0; // The minimum amount required to keep an account open
+export const BLOCK_GAS_LIMIT = 60000000; // 0.75 * 20000_0000_0000 / 25000
