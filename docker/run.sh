6f650fe3b43e03c35a89d9e17b8e5b40d01af7f6
masteray <masteraychen@gmail.com>
2022-11-26 19:30:14 +0800

    fix env file to dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -1,10 +1,10 @@
 NET=$1
 
 CONF_DIR=~/.amax_eva_$NET
-
-[ ! -f "$CONF_DIR/eva.env" ] && mkdir -p $CONF_DIR && cp .env $CONF_DIR/eva.env
-[ -f "$CONF_DIR/eva.env" ] && source $CONF_DIR/eva.env
+ENV_FILE=$CONF_DIR/eva.env
+[ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp .env $ENV_FILE
+[ -f "$ENV_FILE" ] && source $ENV_FILE
 
 mkdir -p $DATA_SHARE
 
-docker-compose up --build -d
+docker-compose up --build -d --env-file $ENV_FILE
