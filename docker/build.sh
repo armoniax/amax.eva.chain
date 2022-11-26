88083a89f52a9c4a7e51772f2ab4118807f609d7
masteray <masteraychen@gmail.com>
2022-11-27 00:19:44 +0800

    update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,9 @@
 NET=$1
 CONF_DIR=~/.amax_eva/$NET
-
 [ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp .env $CONF_DIR/
+
 source $CONF_DIR/.env
-docker build -t $EVA_IMG_TAG .
+docker build -t $EVA_IMG_TAG \
+  --build-arg BUILD_BRANCH=$BUILD_BRANCH \
+  --build-arg PROFILE=$PROFILE \
+  --build-arg IMGTAG=$IMGTAG .
