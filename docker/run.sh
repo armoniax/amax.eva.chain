ad0af6936e2f371ac93af0d8a5e8b02cd4bbfbfe
masteray <masteraychen@gmail.com>
2022-11-27 20:49:38 +0800

    refine run

--- docker/run.sh
+++ docker/run.sh
@@ -1,7 +1,10 @@
 NET=$1
 TAG=$2
 
+mkdir -p ~/.amax_eva/$NET/$TAG
 cd ~/.amax_eva/$NET/$TAG
+[ ! -f ".env" ] && echo "missing .env" && exit 1
+
 source .env && mkdir -p $EVA_SHARE
 
 docker-compose up -d
