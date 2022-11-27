23294008a34d017f3cbe26ba2e923b0146ed8a43
masteray <masteraychen@gmail.com>
2022-11-27 14:31:28 +0800

    add docker-compose.yml into config dir

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,6 @@
 NET=$1
 CONF_DIR=~/.amax_eva/$NET
-[ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp .env $CONF_DIR/
+[ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp .env $CONF_DIR/ && cp docker-compose.yml $CONF_DIR/
 
 source $CONF_DIR/.env
 DOCKER_BUILDKIT=1 docker build -t $IMGTAG \
