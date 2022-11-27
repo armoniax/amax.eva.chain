69de3c3d4a676a084efca2127e91b35e40eb9403
masteray <masteraychen@gmail.com>
2022-11-27 12:58:33 +0800

    add buildkit to enable chmod in docker

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@ CONF_DIR=~/.amax_eva/$NET
 [ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp .env $CONF_DIR/
 
 source $CONF_DIR/.env
-docker build -t $IMGTAG \
+DOCKER_BUILDKIT=1 docker build -t $IMGTAG \
   --build-arg BUILD_BRANCH=$BUILD_BRANCH \
   --build-arg PROFILE=$PROFILE \
   --build-arg IMGTAG=$IMGTAG .
