2ec8b43c9cbd66284e7bd1cf2e16e74b84b70d4b
masteray <masteraychen@gmail.com>
2022-11-29 22:28:03 +0800

    add evm-tracing arg

--- docker/build.sh
+++ docker/build.sh
@@ -6,4 +6,5 @@ source $CONF_DIR/.env
 DOCKER_BUILDKIT=1 docker build -t $IMGTAG \
   --build-arg BUILD_BRANCH=$BUILD_BRANCH \
   --build-arg PROFILE=$PROFILE \
+  --build-arg FEATURES=$FEATURES \
   --build-arg IMGTAG=$IMGTAG .
