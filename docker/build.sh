04d1b35d0470f223dd92177e33c849103a954e33
masteray <masteraychen@gmail.com>
2022-12-01 17:47:01 +0800

    refine build

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,7 @@ NET=$1
 CONF_DIR=~/.amax_eva/$NET
 ENV_FILE=$CONF_DIR/.env
 
-[ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp .env $CONF_DIR/ && cp docker-compose.yml $CONF_DIR/
+[ ! -f "$ENV_FILE" ] && mkdir -p $CONF_DIR && cp "${NET}.env" $CONF_DIR/.env && cp docker-compose.yml $CONF_DIR/
 
 source $CONF_DIR/.env
 DOCKER_BUILDKIT=1 docker build -t $IMGTAG \
